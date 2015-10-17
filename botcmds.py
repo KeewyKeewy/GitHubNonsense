b3f249cfb4f8721e550cbc79eb323e5bc03976ba
cdilday <cdilday@ucsc.edu>
2015-10-17 12:08:51 -0700

    Made a botcmds file for commands specific to the bot

---
+++ botcmds.py
@@ -0,0 +1,15 @@
+# botcmds.py
+# this is the format of cfg.py, but with generic information
+# the real file contains actual login stuff that should never be uploaded
+HOST = "irc.twitch.tv"              # the Twitch IRC server
+PORT = 6667                         # always use port 6667!
+NICK = "xxxxxxxxxxxxxxx"            # your Twitch username, lowercase
+PASS = "oauth:xxxxxxxxxxxxxxxxxxxx" # your Twitch OAuth token
+CHAN = "#xxxxxxx"                   # the channel you want to join
+
+RATE = (20/30) # messages per second
+
+BAN = ['bit.ly', 'goo.gl', '.gu.ma']
+FUCKER = ['FUCKYOUKEEWY','fuckyoukeewy', 'FuckYouKeewy']
+
+HOI = ['hOI', 'HOI', 'hoi', 'hOi', 'Hoi']
